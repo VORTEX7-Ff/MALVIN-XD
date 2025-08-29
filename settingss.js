a230e142b8a6921ca432b0feeecaa3b2b723ab81
VORTEX7-Ff <congoleseplayer@gmail.com>
2025-08-29 12:24:30 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~suE2zISa#cSfgo1HMdbc3q00SiFL-O0klAZZqCUtwEcuQ6RQB2gc",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2421654847", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
